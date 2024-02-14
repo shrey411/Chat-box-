137e1a7dcd9af9493b3aa1d78450545f903e7976
shrey411 <shreypatel015@gmail.com>
2024-02-14 10:18:23 +0530

    New changes

---
+++ src/Components/AddMessage.js
@@ -0,0 +1,191 @@
+// import { GrEmoji } from "react-icons/gr";
+// import React, { useRef, useState } from 'react';
+// import { connect } from 'react-redux';
+// import { addMessage } from '../actions';
+// import Picker from 'emoji-picker-react';
+
+
+// const AddMessage = ({ dispatch }) => {
+//     const [inputMessage, setInputMessage] = useState('');
+//     const [showPicker, setShowPicker] = useState(false);
+//     const inputRef = useRef(null);
+
+//     const sendMessage = () => {
+//         if (inputMessage.trim() !== '') {
+//             dispatch(inputMessage, 'Shrey');
+//             setInputMessage('');
+//         }
+//     };
+
+//     const onEmojiClick = (event) => {
+//         const sym = event.unified.split("-");
+//         const codeArray = sym.map(ele => parseInt(ele, 16));
+//         let emoji = String.fromCodePoint(...codeArray);
+//         setInputMessage(prevInputMessage => prevInputMessage + emoji); 
+//     };
+
+
+//     const handleInputFocus = () => {
+//         setShowPicker(false);
+//     };
+
+
+//     return (
+//         <section id="new-message">
+//             <div className="emoji-container">
+//                 {showPicker &&
+//                     <div className="picker-container">
+//                         <Picker onEmojiClick={onEmojiClick} height={400} width={300} />
+//                     </div>}
+//                 <span onClick={() => setShowPicker(prevState => !prevState)}><GrEmoji className="emoji-icon" /></span>
+//             </div>
+//             <div className="input-container">
+//                 <input
+//                     type="text"
+//                     onChange={e => setInputMessage(e.target.value)}
+//                     value={inputMessage} // Append selected emoji to input value
+//                     ref={inputRef}
+//                     onFocus={handleInputFocus}
+//                     placeholder="Type a message..."
+//                 />
+//             </div>
+//             <div className="send-btn">
+//                 <button onClick={sendMessage}>Send</button>
+//             </div>
+//         </section>
+//     );
+// };
+
+
+// const mapDispatchToProps = dispatch => ({
+//     dispatch: (message, author) => {
+//         dispatch(addMessage(message, author));
+//     },
+// });
+
+// export default connect(() => ({}), mapDispatchToProps)(AddMessage);
+
+
+import { GrEmoji, GrAttachment } from "react-icons/gr";
+import React, { useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Picker from 'emoji-picker-react';
+import { addMessage, addBotReply, addGroupMessage } from "../actions";
+
+const AddMessage = () => {
+    const [inputMessage, setInputMessage] = useState('');
+    const [showPicker, setShowPicker] = useState(false);
+    const inputRef = useRef(null);
+    const [showAttachmentOptions, setShowAttachmentOptions] = useState(false); // State to toggle attachment options
+
+
+    const activeUser = useSelector(state => state.activeUser);
+    const activeGroup = useSelector(state => state.activeGroup);
+    // const users = useSelector(state=> state.users)
+    const dispatch = useDispatch();
+
+    const sendMessage = () => {
+        if (inputMessage.trim() !== '') {
+            console.log('Sending Message:', inputMessage);
+            if (activeUser) {
+
+                dispatch(addMessage(activeUser, inputMessage));
+                const botReply = generateBotReply();
+                dispatch(addBotReply(activeUser, botReply));
+
+            } else if (activeGroup) {
+
+                console.log("active group name:", activeGroup)
+                dispatch(addGroupMessage('user', inputMessage));
+            }
+            setInputMessage('');
+        }
+    };
+
+
+    const onEmojiClick = (event) => {
+        const sym = event.unified.split("-");
+        const codeArray = sym.map(ele => parseInt(ele, 16));
+        let emoji = String.fromCodePoint(...codeArray);
+        setInputMessage(prevInputMessage => prevInputMessage + emoji);
+    };
+
+    const handleInputFocus = () => {
+        setShowPicker(false);
+    };
+
+    const generateBotReply = () => {
+        const replies = [
+            "Hello!",
+            "How are you?",
+            "Nice to meet you!",
+            "What's up?",
+            "I'm a bot!",
+            "How can I assist you?"
+            // Add more replies as needed
+        ];
+        const randomIndex = Math.floor(Math.random() * replies.length);
+        return replies[randomIndex];
+    };
+
+    const handleAttachmentClick = () => {
+        // Implement logic to handle attachment click (e.g., open file picker)
+        setShowAttachmentOptions(prevState => !prevState); // Toggle attachment options
+    };
+    const handleOptionClick = (e) => {
+        const option = e.target.textContent;
+        if (option === 'Add Photo' || option === 'Add Video' || option === 'Add Document') {
+            // Trigger file input click
+            document.getElementById('fileInput').click();
+        }
+    };
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        // Handle the selected file, you can upload it or perform other actions here
+    };
+
+    return (
+        <section id="new-message">
+            <div className="emoji-container">
+                {showPicker &&
+                    <div className="picker-container">
+                        <Picker onEmojiClick={onEmojiClick} height={400} width={300} />
+                    </div>}
+                <span onClick={() => setShowPicker(prevState => !prevState)}><GrEmoji className="emoji-icon" /></span>
+            </div>
+            <div className="attachment-icon" onClick={handleAttachmentClick}>
+                <div className="icon-box">
+                    <span><GrAttachment className="emoji-icon" /></span>
+                </div>
+            </div>
+            {showAttachmentOptions && (
+                <div className="attachment-options-container">
+                    <div className="attachment-option" onClick={handleOptionClick}>Add Photo</div>
+                    <div className="attachment-option" onClick={handleOptionClick}>Add Video</div>
+                    <div className="attachment-option" onClick={handleOptionClick}>Add Document</div>
+                </div>
+            )}
+            <input id="fileInput" type="file" style={{ display: 'none' }} onChange={handleFileChange} />
+            <div className="input-container">
+                <input
+                    type="text"
+                    onChange={e => setInputMessage(e.target.value)}
+                    value={inputMessage} // Append selected emoji to input value
+                    ref={inputRef}
+                    onFocus={handleInputFocus}
+                    placeholder="Type a message..."
+                />
+            </div>
+            <div className="send-btn">
+                <button onClick={sendMessage}>Send</button>
+            </div>
+        </section>
+    );
+};
+
+
+export default AddMessage;
+
+
+
---
+++ src/actions/index.js
@@ -0,0 +1,82 @@
+
+// export const ADD_MESSAGE = 'ADD_MESSAGE'
+export const MESSAGE_RECEIVED = 'MESSAGE_RECEIVED'
+export const ADD_USER = 'ADD_USER'
+export const USERS_LIST = 'USERS_LIST'
+export const SELECT_USER = 'SELECT_USER';
+
+
+
+let nextMessageId = 0 ;
+let nextUserId = 0;
+
+// export const addMessage = (message,author) =>({
+//     type: ADD_MESSAGE ,
+//     id: nextMessageId++,
+//     message,
+//     author
+// })
+
+export const addUser = name =>({
+    type: ADD_USER ,
+    id: nextUserId++,
+    name
+})
+
+export const messageReceived = (message,author) =>({
+    type: MESSAGE_RECEIVED ,
+    id: nextMessageId++,
+    message,
+    author
+})
+
+export const UsersList = users =>({
+    type: USERS_LIST ,
+    users
+})
+
+// redux/actions.js
+
+
+export const SET_ACTIVE_USER = 'SET_ACTIVE_USER';
+export const SET_ACTIVE_GROUP = 'SET_ACTIVE_GROUP';
+
+export const ADD_USER_MESSAGE = 'ADD_USER_MESSAGE';
+export const ADD_BOT_REPLY = 'ADD_BOT_REPLY';
+export const CREATE_GROUP = 'CREATE_GROUP';
+export const ADD_GROUP_MESSAGE = 'ADD_GROUP_MESSAGE'; // New action type for adding group messages
+
+
+export const setActiveUser = (user) => ({
+  type: SET_ACTIVE_USER,
+  payload: user,
+});
+
+export const addMessage = (userId, message ) => ({
+  type: ADD_USER_MESSAGE,
+  payload: { userId, message },
+});
+
+export const addBotReply = (userId, message) => ({
+  type: ADD_BOT_REPLY,
+  payload: {
+    userId,
+    message,
+  },
+});
+
+export const addGroupMessage = (author, message) => ({
+  type: ADD_GROUP_MESSAGE,
+  payload: { author, message },
+});
+
+
+export const createGroup = (groupData) => ({
+  type: CREATE_GROUP,
+  payload: groupData
+});
+
+export const setActiveGroup = (group) => ({
+  type: SET_ACTIVE_GROUP,
+  payload: group,
+});
